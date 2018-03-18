a02d86f60123428d7ff535882f7aaa8a9681f414
yangzi6666 <784144498@qq.com>
2018-03-18 15:41:13 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/SplashActivity.java
@@ -0,0 +1,49 @@
+package cn.edu.gdmec.android.boxuegu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class SplashActivity extends AppCompatActivity {
+private TextView tv_version;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //设置屏幕为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //获取版本号
+        tv_version = findViewById(R.id.tv_version);
+
+        try {
+            //获取程序包信息
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText("V"+info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+//延迟3秒跳转
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+            timer.schedule(task,3000);
+
+    }
+}
